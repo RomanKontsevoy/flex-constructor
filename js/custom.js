7467554af4efb11e0e8814e3086b26bb0a3586b3
RomanKontsevoy <1604881@gmail.com>
2017-10-17 16:32:05 +0300

    flex-basis ready

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,15 @@
 $(function() {
 
-    let width, height, color, newBorderRadius;
+    let width, height, color, newBorderRadius, order, flexBasis;
 
     let setSize = function() {
-        // let w, h;
-        width = newWidth.value;
-        height = newHeight.value;
+        if (newWidth.value > 20 && newHeight.value > 20) {
+            width = newWidth.value;
+            height = newHeight.value;
+        } else {
+            alert('К сожалению, ширина и высота элемента не могут быть менее 20px.')
+        }
+
     };
 
     let setFigure = function() {
@@ -15,13 +19,50 @@ $(function() {
                 newBorderRadius = figures[i].value;
             }
         }
-        console.log('newBorderRadius = ' + newBorderRadius);
-
     };
 
     let setColor = function() {
         color = newColor.value;
     };
+
+    let setOrder = function() {
+        let orderRadios = document.getElementsByName("order");
+        newOrderValue.value = newOrder.value;
+        for (var i = 0; i < orderRadios.length; i++) {
+            if (orderRadios[i].checked) {
+                order = orderRadios[i].value;
+            };
+        };
+    };
+
+    let flexBasisCount = function() {
+        let flexBasisRadios = document.getElementsByName("flex-basis");
+        newFlexBasisValue.value = newFlexBasis.value;
+        for (var i = 0; i < flexBasisRadios.length; i++) {
+            if (flexBasisRadios[i].checked) {
+                flexBasis = flexBasisRadios[i].value;
+            };
+        };
+    };
+
+    let measure = function(a) {
+        let measureSelect = document.getElementById(a);
+        console.log(measureSelect);
+        console.log(measureSelect[0].value);
+        console.log(measureSelect.length);
+        for (var i = 0; i < measureSelect.length; i++) {
+            if (measureSelect[i].selected) {
+                flexBasisMeasure = measureSelect[i].value;
+            };
+        };
+    }
+
+    let flexBasisValue = function(a) {
+        flexBasisCount();
+        measure(a);
+        flexBasis = flexBasis + flexBasisMeasure;
+        console.log(flexBasis);
+    }
 
 
     let make = function(e, i) {
@@ -31,10 +72,13 @@ $(function() {
             setSize();
             setFigure();
             setColor();
+            setOrder();
             let ind = ++i;
             element.className = '.el';
-            element.style.minWidth = width + 'px';
-            element.style.minHeight = height + 'px';
+            element.style.width = width + 'px';
+            element.style.height = height + 'px';
+            element.style.minWidth = '20px';
+            element.style.minHeight = '20px';
             element.style.backgroundColor = color;
             let numer = document.createElement('p');
             numer.innerHTML = ind;
@@ -55,21 +99,20 @@ $(function() {
             element.style.margin = '1px';
             element.style.color = '#f00';
             element.style.fontSize = "25px";
-            if (newBorderRadius = "circle") {
+            if (newBorderRadius == "circle") {
                 element.style.borderRadius = "50%";
             } else {
                 element.style.borderRadius = "0";
-            }
-
-            console.log('newBorderRadius = ' + newBorderRadius);
-
+            };
             target.appendChild(element);
+            element.style.order = order;
+            flexBasisValue("flexBasisMeasure");
+            element.style.flexBasis = flexBasis;
         };
     };
 
     let create = function(e) {
         let elements = document.getElementsByClassName('.el');
-        console.log(elements);
         if (elements.length <= 19) {
             if (e = testarea) {
                 make(e, elements.length);
@@ -78,12 +121,11 @@ $(function() {
         } else {
             alert('Извините, создание более двадцати фигур невозможно!');
         };
-
-
     };
 
-    testarea.addEventListener('click', create);
 
+
+    testarea.addEventListener('click', create);
 
 
     let display = function() {
@@ -93,7 +135,6 @@ $(function() {
                 testarea.style.display = displayRadios[i].value;
             };
         };
-        console.log(testarea.style.display)
     };
 
     let justifyContent = function() {
@@ -103,7 +144,6 @@ $(function() {
                 testarea.style.justifyContent = justifyContent[i].value;
             }
         }
-        console.log(testarea.style.justifyContent)
     }
 
     let alignItems = function() {
@@ -113,7 +153,6 @@ $(function() {
                 testarea.style.alignItems = alignItems[i].value;
             }
         }
-        console.log(testarea.style.alignItems)
     }
 
     let flexDirection = function() {
@@ -123,7 +162,6 @@ $(function() {
                 testarea.style.flexDirection = flexDirection[i].value;
             }
         }
-        console.log(testarea.style.flexDirection)
     }
 
     let flexWrap = function() {
@@ -133,7 +171,6 @@ $(function() {
                 testarea.style.flexWrap = flexWrap[i].value;
             }
         }
-        console.log(testarea.style.flexWrap)
     }
 
     let alignContent = function() {
@@ -143,7 +180,6 @@ $(function() {
                 testarea.style.alignContent = alignContent[i].value;
             }
         }
-        console.log(testarea.style.alignContent)
     }
 
     let acceptParent = function() {
@@ -157,5 +193,13 @@ $(function() {
 
     parentStylesBtn.addEventListener('click', acceptParent);
 
+    // Функция прописывания значения цвета
+
+    let setHtmlValues = function() {
+        document.getElementById('newColorValue').innerHTML = " " + newColor.value;
+    }
+
+    window.addEventListener("load", setHtmlValues); // Прописать значение цвета при загрузке страницы
+    newColor.addEventListener('change', setHtmlValues); // Прописать значение цвета после его изменения
 
 });
